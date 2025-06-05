5a1cb0d674966d39ad761f576d376bb2bc28863e
Mariya Tihonova <malafeeva.mariya@list.ru>
2025-06-05 15:25:51 +0300

    project card2

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,38 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const template = document.querySelector('#card-template').content;
+
+function removeCard(cardElement) {
+  cardElement.remove(); // Удаляет элемент карточки из DOM
+}
+
+function createCard(cardData, onDelete) {
+  const clonedTemplate = template.querySelector('.card').cloneNode(true);
+
+  // Получаем вложенные элементы
+  const titleElement = clonedTemplate.querySelector(".card__title");
+  const imageElement = clonedTemplate.querySelector(".card__image");
+
+  // Устанавливаем значения
+  titleElement.textContent = cardData.name;
+  imageElement.alt = cardData.name;
+  imageElement.src = cardData.link;
+
+  // Добавляем обработчик клика на карточку
+  const deleteIcon = clonedTemplate.querySelector(".card__delete-button");
+  deleteIcon.addEventListener("click", () => {
+    onDelete(clonedTemplate); // Вызываем колбэк с элементом карточки
+  });
+
+  return clonedTemplate;
+}
+
+// Перебор карточек
+const placesList = document.querySelector(".places__list");
+
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData, removeCard); // Передаём removeCard в качестве колбека
+  placesList.appendChild(card);
+});
