cfa0d680012de5948dd0ecdb50af5a827d0b20dd
MariyaTihonova <malafeeva.mariya@list.ru>
2025-06-28 15:21:58 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import cardOne from './src/images/card_1.jpg';
-import cardTwo from './src/images/card_2.jpg';
-import cardThree from './src/images/card_3.jpg';
-
-const initialCards = [
-  { name: 'Card One', link: cardOne },
-  { name: 'Card Two', link: cardTwo },
-  { name: 'Card Three', link: cardThree }
-]
